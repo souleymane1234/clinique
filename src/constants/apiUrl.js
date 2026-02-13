83c52466663434d097c761edb49586456ae6cd80
souleymane1234 <konatesouleymane42@gmail.com>
2026-02-13 17:07:59 +0000

    Update API base URL to use HTTPS for improved security

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,4 +1,4 @@
-const base_url = import.meta.env.VITE_BASE_URL || 'http://centremedical.preventic-afric.com:3000/api';
+const base_url = import.meta.env.VITE_BASE_URL || 'https://api.centremedical.preventic-afric.com/api';
 // const base_url = 'https://api.annour-travel.com';
 // const base_url = 'http://localhost:3010';
 const base_url_asset = import.meta.env.VITE_BASE_URL_ASSET;
